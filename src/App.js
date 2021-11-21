816d2fcc923c85774bc7b5c620a38e5c306a6e4f
E1aliu <endritaliu09@gmail.com>
2021-11-21 12:07:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { lazy, Suspense, useEffect } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom';
+import Loading from './components/shared/Loading/Loading';
+import ScrollToTop from './ScrollToTop';
+import './App.scss';
+
+
+const Home = lazy(() => import('./pages/Home/Home'));
+
+
+function App() {
+
+
+  return (
+    <Router>
+    <Suspense fallback={<Loading />}>
+      <ScrollToTop />
+        <Switch>
+        <Route exact path='/' component={Home} />
+        <Redirect to='/' />
+      </Switch>
+    </Suspense>
+  </Router>
+  );
+}
+
+export default App;
+
